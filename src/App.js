2777b66c251e1a57c1fdf928b0dc040cd82c915e
Andrei Pavalachi <apavalac@Andreis-MacBook-Air.local>
2022-02-11 18:16:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const Hatspage = () => (
   <div>
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={Hatspage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
